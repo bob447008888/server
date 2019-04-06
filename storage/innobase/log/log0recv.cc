a32611630e0e76e6aa0dc636d37859880c088040
Marko Mäkelä <marko.makela@mariadb.com>
2019-04-06 12:24:36 +0300

    Clean up the parsing of MLOG_INIT_FILE_PAGE2
    fsp_apply_init_file_page(): Renamed from fsp_init_file_page_low().
    
    fsp_parse_init_file_page(): Remove. The redo log record has no
    parameters.

--- storage/innobase/log/log0recv.cc
+++ storage/innobase/log/log0recv.cc
@@ -1787,7 +1787,7 @@ parse_log:
 		break;
 	case MLOG_INIT_FILE_PAGE2:
 		/* Allow anything in page_type when creating a page. */
-		ptr = fsp_parse_init_file_page(ptr, end_ptr, block);
+		if (block) fsp_apply_init_file_page(block);
 		break;
 	case MLOG_WRITE_STRING:
 		ptr = mlog_parse_string(ptr, end_ptr, page, page_zip);
